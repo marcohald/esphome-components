e0bf798e1d7cc11b40040f2b1bd2f767cca75a25
Marco Hald <marcohald97@gmail.com>
2024-03-02 01:48:43 +0100

    fixed sensor

--- components/bootcount/sensor.py
+++ components/bootcount/sensor.py
@@ -1,26 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import (
-    ENTITY_CATEGORY_DIAGNOSTIC,
-    STATE_CLASS_TOTAL_INCREASING,
-    UNIT_SECOND,
-    ICON_TIMER,
-    DEVICE_CLASS_DURATION,
-)
+from esphome.const import *
 
 debug_ns = cg.esphome_ns.namespace("debug")
 BootcountSensor = debug_ns.class_("BootcountSensor", sensor.Sensor, cg.PollingComponent)
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     BootcountSensor,
-    unit_of_measurement=UNIT_SECOND,
-    icon=ICON_TIMER,
+    unit_of_measurement=UNIT_EMPTY,
+    icon=ICON_EMPTY,
     accuracy_decimals=0,
-    state_class=STATE_CLASS_TOTAL_INCREASING,
-    device_class=DEVICE_CLASS_DURATION,
-    entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-).extend(cv.polling_component_schema("60s"))
+    state_class=STATE_CLASS_TOTAL,
+    device_class=DEVICE_CLASS_EMPTY,
+    entity_category=ENTITY_CATEGORY_NONE,
+)
 
 
 async def to_code(config):
